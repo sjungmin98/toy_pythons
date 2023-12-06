e021fcad55fe01aaa34fe0a4413d6b00d2ca2e77
sjungmin98 <shc980@gmail.com>
2023-12-06 16:53:24 +0900

    first commit

---
+++ polly.py
@@ -0,0 +1,27 @@
+# list_question = [
+#     "상품의 품질에 대해 어떻게 생각하시나요?",
+#     "상품의 가격에 대해 어떻게 생각하시나요?",
+#     "상품의 디자인에 대해 어떻게 생각하시나요?",
+#     "상품에 대한 전반적인 만족도는 어떠신가요?"
+# ]
+
+# list_answer =  ["좋음", "중간", "좋아지길"]
+
+list_question = [
+    "상품의 품질에 대해 어떻게 생각하시나요?",
+    "상품의 가격에 대해 어떻게 생각하시나요?",
+    "상품의 디자인에 대해 어떻게 생각하시나요?",
+    "상품에 대한 전반적인 만족도는 어떠신가요?"
+]
+
+list_answer = ["좋음", "중간", "좋아지길"]
+
+for question in list_question:
+    print("{}".format(question))
+    for answer in [list_answer]:
+       print("1.{}".format(list_answer[0], end=" , "))
+       print("2.{}".format(list_answer[1], end=" , "))
+       print("3.{}".format(list_answer[2]))
+    print("\n-----------------")
+
+print("End program!")
