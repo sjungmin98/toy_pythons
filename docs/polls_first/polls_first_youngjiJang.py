5001db0d8ab5a0a29ad70d345e101e0ca0449431
youngji Jang <btg1631@naver.com>
2023-12-07 09:13:03 +0900

    polls_first

--- docs/polls_first/polls_first_youngjiJang.py
+++ docs/polls_first/polls_first_youngjiJang.py
@@ -17,16 +17,6 @@ for num_count_question in [0, 1, 2, 3]:
         print("\n-----------------")
 
 
-
-# list_question = [
-#     "상품의 품질에 대해 어떻게 생각하시나요?",
-#     "상품의 가격에 대해 어떻게 생각하시나요?",
-#     "상품의 디자인에 대해 어떻게 생각하시나요?",
-#     "상품에 대한 전반적인 만족도는 어떠신가요?"
-# ]
-
-# list_answer =  ["좋음", "중간", "좋아지길"]
-
 # for num_count_question in range(4):
 #     print("{}.{}".format(num_count_question+1, list_question[num_count_question]))
 #     for num_count_answer in range(3):
