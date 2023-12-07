ca4ff90192bf92b1d6ddd3b894f2fac2e91675e1
youngji Jang <btg1631@naver.com>
2023-12-07 10:23:43 +0900

    polls_second B

--- docs/polls_second copy/polls_second_youngjijang.py
+++ docs/polls_second copy/polls_second_youngjijang.py
@@ -18,13 +18,32 @@ list_question = [
 
 list_answer = ["좋음", "중간", "좋아지길"]
 
+list_result = [0, 0, 0]
+
 for question in [0, 1, 2, 3] :
     question_a = list_question[question] 
     print("{}. {}".format(question+1, question_a))
     for answer in [0, 1, 2] :
         answer_a = list_answer[answer]
         print("{}. {}".format(answer+1, answer_a), end="  ")
+
     print("")
+    print("")
+    question_result = input("당신의 생각은 몇 번 : ")
+    num_question_result = int(question_result)
+    index = num_question_result - 1
+    list_result[index] = list_result[index] + 1
+        
     if question < 3 :
         print("-----------------")
+
+result = ((list_result[0] * 3) + (list_result[1] * 2) + (list_result[2] * 1)) / (3+2+1)
+
+print("")
+print("—--- 통 계 ----")
+print("설문자 답항별 갯수 표시 : {}".format(list_result))
+print("답변별 가중치 (좋음:3, 중간:2, 좋아지길:1)")
+print("답항 가중 평균 : {}".format(result))
+
+
 
