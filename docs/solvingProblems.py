2218cf2fbed78aba9e2bd80f881893c9b2b6c1f9
sjungmin98 <shc980@gmail.com>
2023-12-08 13:20:15 +0900

    final

--- docs/solvingProblems.py
+++ docs/solvingProblems.py
@@ -1,56 +1,65 @@
-# 출제 문제
+# # 출제 문제
+# list_problems = [
+#         'Python에서 변수를 선언하는 방법은? (점수: 10점)',
+#         '1) var name 2) name = value 3) set name 4) name == value',
+# 		'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)',
+#         '1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다', 
+#         'Python에서 조건문을 작성하는 방법은? (점수: 10점)', 
+#         '1) if-else, 2) for-in, 3) while, 4) def',
+#         'Python에서 함수를 정의하는 방법은? (점수: 5점)', 
+#         '1) class, 2) def, 3) import, 4) return'
+# ]
+# # 문제 당 정답
+# list_corrects = [2, 1, 1, 2]
+def problems_main(questions, correct_answers) :
+    results = [0, 0, 0, 0]
+    problems_first = questions[1], questions[3], questions[5], questions[7]
+    problems_second = questions[0], questions[2], questions[4], questions[6]
+    list_results = []
+
+    for question in [0, 1, 2, 3]:
+        question_a = problems_second[question]
+        question_b = problems_first[question]
+
+        print("{}. {}".format(question + 1, question_a))
+        print("{}".format(question_b))
+
+        question_result = input("-정답 : ")
+        num_question_result = int(question_result)
+        index = num_question_result - 1
+        results[index] = results[index] + 1
+
+        list_results.append(question_result)
+
+    return results, [int(i) for i in list_results]
+
 list_problems = [
-        'Python에서 변수를 선언하는 방법은? (점수: 10점)',
-        '1) var name 2) name = value 3) set name 4) name == value',
-		'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)',
-        '1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다', 
-        'Python에서 조건문을 작성하는 방법은? (점수: 10점)', 
-        '1) if-else, 2) for-in, 3) while, 4) def',
-        'Python에서 함수를 정의하는 방법은? (점수: 5점)', 
-        '1) class, 2) def, 3) import, 4) return'
+    'Python에서 변수를 선언하는 방법은? (점수: 10점)',
+    '1) var name 2) name = value 3) set name 4) name == value',
+    'Python에서 리스트(List)의 특징은 무엇인가요? (점수: 15점)',
+    '1) 순서가 있고 변경 가능하다, 2) 중복된 값을 가질 수 없다, 3) 원소를 추가하거나 삭제할 수 없다, 4) 정렬된 상태로 유지된다',
+    'Python에서 조건문을 작성하는 방법은? (점수: 10점)',
+    '1) if-else, 2) for-in, 3) while, 4) def',
+    'Python에서 함수를 정의하는 방법은? (점수: 5점)',
+    '1) class, 2) def, 3) import, 4) return'
 ]
-# 문제 당 정답
+
 list_corrects = [2, 1, 1, 2]
 
+results, list_results = problems_main(list_problems, list_corrects)
 
 
-list_result = [0, 0, 0, 0]
-list_problems_first = list_problems[1], list_problems[3], list_problems[5], list_problems[7]
-list_problems_second = list_problems[0], list_problems[2], list_problems[4], list_problems[6]
-list_list = []
-for question in [0, 1, 2, 3] :
-    question_a = list_problems_second[question] 
-    question_b = list_problems_first[question]
-
-    print("{}. {}".format(question+1, question_a))
-    print("{}".format(question_b))
-
-    for corrects in [0, 1, 2] :
-        corrects_a = list_corrects[corrects]
-        
-    question_result = input("-정답 : ")
-    num_question_result = int(question_result)
-    index = num_question_result - 1
-    list_result[index] = list_result[index] + 1
-
-    list_list.append(question_result)
-
-
-input_temp = [int(i) for i in list_list]
-
-
-def total_responses(score):
-
+def total_responses(list_results):
     score = 0
 
     # 점수 합계
-    if input_temp[0] == 2:
+    if list_results[0] == 2:
         score += 10
-    if input_temp[1] == 1:
+    if list_results[1] == 1:
         score += 15
-    if input_temp[2] == 1:
+    if list_results[2] == 1:
         score += 10
-    if input_temp[3] == 2:
+    if list_results[3] == 2:
         score += 5
 
     # 학점
@@ -62,10 +71,9 @@ def total_responses(score):
         result = "F"
     
     print("—----- 결과 —-------------")
-    print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}, 4번 {}".format(input_temp[0], input_temp[1], input_temp[2], input_temp[3]))
+    print("응답한 내용 : 1번 {}, 2번 {}, 3번 {}, 4번 {}".format(list_results[0], list_results[1], list_results[2], list_results[3]))
     print("당신 응답 합계 : {}점".format(score))
     print("학점은 {} 입니다.".format(result))
 
-
-total_responses(input_temp)
+total_responses(list_results)
 
